1b84df03a6fd66551a354e7952bc8d09d0f5e445
Maria Vittoria Dalla Rosa Prati <mvdallarosa@gmail.com>
2020-11-03 17:24:43 +0100

    added fortm to update form json attribute to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,15 @@ class User < ApplicationRecord
   has_many :favorites
   has_one :footprint
   has_one_attached :photo
-  validates_format_of :first_name, :with => /[a-z\s.-]/i, presence: true
-  validates :last_name, :with => /[a-z\s.-]/i, presence: true
-  validates :username, presence: true, uniqueness: true
+  # validates_format_of :first_name, :with => /[a-z\s.-]/i, presence: true
+  # validates :last_name, :with => /[a-z\s.-]/i, presence: true
+  # validates :username, presence: true, uniqueness: true
   validates :city, presence: true
 
   def current_challenge
-    date = Date.today
-    current_commitment = current_user.commitments.where(['start_date < ? AND end_date > ?', date, date])
-    return current_commitment.challenge
+    # date = Date.today
+    # current_commitment = current_user.commitments.where(['start_date < ? AND end_date > ?', date, date])
+    # return current_commitment.challenge
+    current_user.commitments.last
   end
 end
