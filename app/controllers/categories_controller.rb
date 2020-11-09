5aa27b75a4b026c44aceb7447e441ec1bee7a45b
Lily Walch <lily.walch@gmail.com>
2020-11-09 20:18:09 +0100

    search bar

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,23 @@
 class CategoriesController < ApplicationController
+  # def index
+  #   @categories = Category.all
+
   def index
     @categories = Category.all
+
   end
+
+  # end
 
   def show
     @category = Category.find(params[:id])
-    @items = @category.items
+    @items = Item.where(category: @category)
+    # @items = @category.items
+    # @category = Category.where(name: 'plant-based recipes')
+    if params[:query].present?
+      sql_query = "name ILIKE :query OR description ILIKE :query"
+      @items = @items.where(sql_query, query: "%#{params[:query]}%")
+    end
+
   end
 end
