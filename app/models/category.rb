1e5bdb0e5d78c87d66bf8d7199bcc622a2c14286
Katrina Robles <katrinarobles@live.com>
2020-11-10 21:34:04 +0100

    REFACTORED FAVORITE METHOD along with dashboard and category show, fixed navbar

--- app/models/category.rb
+++ app/models/category.rb
@@ -11,16 +11,6 @@ class Category < ApplicationRecord
 #   { name: "seasonal foods"},
 #   { name: "farmers markets"}
 # ]
-  def file_index
-    case name
-      when "shop responsibly"
-        return 'categories/shops_index'
-      when "plant-based recipes"
-        return 'categories/recipes_index'
-      else #still need to add the other categories in here and will probably move in the data base
-        return "TRY TO BUY LESS!"
-    end
-  end
 
   def css_class
     case name
