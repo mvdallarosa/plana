fa5757f8fbb85d4db2b34f7f81c3e79b56df965a
Maria Vittoria Dalla Rosa Prati <mvdallarosa@gmail.com>
2020-11-10 14:49:13 +0100

    fixed map & algolia js

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,11 +1,11 @@
 import mapboxgl from 'mapbox-gl';
 // import MapboxDirections from "mapbox-gl-directions";
 
-const fitMapToMarkers = (map, markers) => {
-  const bounds = new mapboxgl.LngLatBounds();
-  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
-};
+// const fitMapToMarkers = (map, markers) => {
+//   const bounds = new mapboxgl.LngLatBounds();
+//   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+//   map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+// };
 
 
 const initMapbox = () => {
