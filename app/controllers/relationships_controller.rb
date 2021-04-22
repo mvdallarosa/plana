73b5085a17655ed11fffa932d60a7c07bd3805e8
Maria Vittoria Dalla Rosa Prati <mvdallarosa@gmail.com>
2021-04-22 18:52:54 +0200

    added typeform subscribe + bg color

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,14 +2,14 @@ class RelationshipsController < ApplicationController
   def follow_user
     @relationship = Follow.new(user_id: current_user.id, following_id: params['user_id'].to_i)
     if @relationship.save!
-      redirect_to :back
+      redirect_back
     end
   end
 
   def unfollow_user
     @relationship = Follow.where(user_id: current_user.id, following_id: params['user_id'].to_i).first
     if @relationship.destroy
-    redirect_to :back
+      redirect_back
     end
   end
 
